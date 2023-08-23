36fd1442b01323d48baa1627d229d8ad741b4537
suisosuii <hayatokomiyama@icloud.com>
2023-08-23 14:58:46 +0900

    チャット可能

--- resuba.mjs
+++ resuba.mjs
@@ -1,18 +1,38 @@
 // HTTP APIを実行しやすくするためのライブラリ: Axios
 import axios from "axios";
-
+import OpenAI from 'openai';
+const OPENAI_API_KEY = "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";
+const openai = new OpenAI({
+    apiKey: OPENAI_API_KEY, // defaults to process.env["OPENAI_API_KEY"]
+  });
+  
 class Resuba {
-  async debateAI(replyToken, OPENAI_API_KEY) {
-    const { Configuration, OpenAIApi } = require("openai");
-    const configuration = new Configuration({
-        apiKey: OPENAI_API_KEY,
+    constructor(channelSecret) {
+        this.api = new axios.create({
+          baseURL: "https://api.line.me/v2",
+          headers: {
+            Authorization: `Bearer ${channelSecret}`,
+            "Content-Type": "application/json",
+          },
+        });
+      }
+      
+  async debateAI(replyToken,message) {
+    const completion = await openai.chat.completions.create({
+        messages: [{ role: 'user', content: message }],
+        model: 'gpt-3.5-turbo',
       });
-      const openai = new OpenAIApi(configuration);
-    const completion = await openai.createChatCompletion({
-        model: "gpt-3.5-turbo",
-        messages: [{ role: "user", content: message }],
-      });
-      await lineApi.replyMessage(replyToken, completion.data.choices[0].message.content);
+      const body = {
+        replyToken,
+        messages: [
+          {
+            type: "text",
+            text: completion.choices[0].message.content,
+          },
+        ],
+      };
+  
+      return await this.api.post("/bot/message/reply", body);
   }
 }
 
